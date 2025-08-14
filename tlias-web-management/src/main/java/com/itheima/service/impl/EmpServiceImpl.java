b883f40495095c21dfc9eb3b4bf64818845a1e6c
Zuning Wang <wzuning@gmail.com>
2025-08-14 02:15:48 -0400

    员工管理分页查询(pageHelper)

--- tlias-web-management/src/main/java/com/itheima/service/impl/EmpServiceImpl.java
+++ tlias-web-management/src/main/java/com/itheima/service/impl/EmpServiceImpl.java
@@ -1,6 +1,8 @@
 package com.itheima.service.impl;
 
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.itheima.mapper.EmpMapper;
 import com.itheima.pojo.Emp;
 import com.itheima.pojo.PageResult;
@@ -16,13 +18,23 @@ public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpMapper empMapper;
 
+    //原始分页查询实现
+//    @Override
+//    public PageResult<Emp> page(Integer page, Integer pageSize) {
+//        Long total = empMapper.count();
+//
+//        Integer start = (page - 1) * pageSize;
+//        List<Emp> rows = empMapper.list(start, pageSize);
+//
+//        return new PageResult<Emp>(total, rows);
+//    }
     @Override
     public PageResult<Emp> page(Integer page, Integer pageSize) {
-        Long total = empMapper.count();
+        PageHelper.startPage(page, pageSize);
 
-        Integer start = (page - 1) * pageSize;
-        List<Emp> rows = empMapper.list(start, pageSize);
+        List<Emp> empList = empMapper.list();
+        Page<Emp> p = (Page<Emp>) empList;
 
-        return new PageResult<Emp>(total, rows);
+        return new PageResult<Emp>(p.getTotal(), p.getResult());
     }
 }
